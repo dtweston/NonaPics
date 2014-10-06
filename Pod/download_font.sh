e106cfc89b35963b43b8926c98e647d1fdd0a3fb
Dave Weston <dave.weston@binocracy.com>
2014-10-05 23:31:35 -0700

    Fix download script to create directory if it doesn't exist.

--- Pod/download_font.sh
+++ Pod/download_font.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-cd Assets
+DEST = "Pod/Assets"
+
+if [ ! -d "$DEST" ]; then
+  mkdir "$DEST"
+fi
+cd "$DEST"
+
 curl -L -O -# https://github.com/github/octicons/raw/master/octicons/octicons.ttf
